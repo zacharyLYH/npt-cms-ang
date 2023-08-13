a10cf4ea61b4895f6f71066431097616880226ce
Zac <yihongle@buffalo.edu>
2023-08-13 11:33:04 +0800

    refactor population code

--- src/app/decoder.service.ts
+++ src/app/decoder.service.ts
@@ -9,30 +9,33 @@ export class DecoderService {
   decodeAndPopulate(content: string) {
     try {
       const data = JSON.parse(content);
-      data.forEach((item: any) => {
-        if (item.type) {
-          if (item.type === 'Bio') {
-            this.contentStorage.bioData = item;
-          }
-          if (item.type === 'Commendation') {
-            this.contentStorage.commendationData.push(item);
-          }
-          if (item.type === 'Experience') {
-            this.contentStorage.experienceData.push(item);
-          }
-          if (item.type === 'Footer') {
-            this.contentStorage.footerData = item;
-          }
-          if (item.type === 'Project') {
-            this.contentStorage.projectData.push(item);
-          }
-          if (item.type === 'Stats') {
-            this.contentStorage.statsData.push(item);
-          }
-        }
-      });
+      this.populate(data);
     } catch (error) {
       alert('Failed to process this JSON with error: ' + error);
     }
+  }
+  populate(data: any[]) {
+    data.forEach((item: any) => {
+      if (item.type) {
+        if (item.type === 'Bio') {
+          this.contentStorage.bioData = item;
+        }
+        if (item.type === 'Commendation') {
+          this.contentStorage.commendationData.push(item);
+        }
+        if (item.type === 'Experience') {
+          this.contentStorage.experienceData.push(item);
+        }
+        if (item.type === 'Footer') {
+          this.contentStorage.footerData = item;
+        }
+        if (item.type === 'Project') {
+          this.contentStorage.projectData.push(item);
+        }
+        if (item.type === 'Stats') {
+          this.contentStorage.statsData.push(item);
+        }
+      }
+    });
   }
 }
